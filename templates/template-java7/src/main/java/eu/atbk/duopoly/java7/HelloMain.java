b6e7dd97cc88fb6213a1d1a09237f9fe1df7f468
jede <jede@esko.com>
2016-02-27 11:17:01 +0100

    the whole try catch thing wasn't needed method was a standard part of the logback (though slow they say) oops :P
    Anyhow, clean setup now.

--- templates/template-java7/src/main/java/eu/atbk/duopoly/java7/HelloMain.java
+++ templates/template-java7/src/main/java/eu/atbk/duopoly/java7/HelloMain.java
@@ -2,9 +2,6 @@ package eu.atbk.duopoly.java7;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-import org.slf4j.MDC.MDCCloseable;
-import static eu.atbk.duopoly.logging.Constants.METHOD;
 
 public class HelloMain {
 
@@ -14,34 +11,28 @@ public class HelloMain {
 	private static final Logger log = LoggerFactory.getLogger(HelloMain.class);
 
 	public static void main(String[] args) {
-		// start with binding the method name
-		// so we can easiliy corelate logging statements
-		// use a "try with resources" so we don't have to bother
-		// with closing the MCD entry
-		try (MDCCloseable mdc = MDC.putCloseable(METHOD, "main")) {
-			log.info("Hello main");
-			other();
-			log.info("The end");
-		}
+		log.info("Hello main");
+		other();
+		log.info("The end");
+
 	}
 
 	@SuppressWarnings("all")
 	public static void other() {
-		try (MDCCloseable mdc = MDC.putCloseable(METHOD, "other")) {
-			log.info("Hello other");
-			try {
-				Object x = null;
-				x.toString();
-			} catch (NullPointerException e) {
-				// always log exception (even when unexpected)
-				log.error("aj", e);
-			}
 
-			// log levels are trace, debug, info, warn, error
-			// in the config file src/main/resources/logback.xml
-			// defining a level there will, lower level statements
-			// will not be logged
-			log.trace("secret");
+		log.info("Hello other");
+		try {
+			Object x = null;
+			x.toString();
+		} catch (NullPointerException e) {
+			// always log exception (even when unexpected)
+			log.error("aj", e);
 		}
+
+		// log levels are trace, debug, info, warn, error
+		// in the config file src/main/resources/logback.xml
+		// defining a level there will, lower level statements
+		// will not be logged
+		log.trace("secret");
 	}
 }
