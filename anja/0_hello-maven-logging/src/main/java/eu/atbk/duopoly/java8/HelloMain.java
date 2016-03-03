06260b2f78716b6a6d39671316296cf06d61dd1e
AnjaPolet <AnjaPolet@users.noreply.github.com>
2016-03-03 15:05:28 +0100

    anja_0

---
+++ anja/0_hello-maven-logging/src/main/java/eu/atbk/duopoly/java8/HelloMain.java
@@ -0,0 +1,45 @@
+package eu.atbk.duopoly.java8;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMain {
+
+	/**
+	 * Always initialize the logger as private static final
+	 */
+	private static final Logger log = LoggerFactory.getLogger(HelloMain.class);
+
+	public static void main(String[] args) {
+		log.info("Hello main");
+		other();
+		anja("Hello");
+		log.info("The end");
+
+	}
+
+	@SuppressWarnings("all")
+	public static void other() {
+
+		log.info("Hello other");
+		try {
+			Object x = null;
+			x.toString();
+		} catch (NullPointerException e) {
+			// always log exception (even when unexpected)
+			log.error("aj", e);
+		}
+
+		// log levels are trace, debug, info, warn, error
+		// in the config file src/main/resources/logback.xml
+		// defining a level there will, lower level statements
+		// will not be logged
+		log.trace("secret {}",123);
+	}
+	
+	public static void anja(String s){
+		log.info("Hello anja");
+		System.out.println(s);
+		log.trace("end anja", s);
+	}
+}
